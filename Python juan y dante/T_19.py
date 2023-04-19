3eb9fae0b13b761cbb0b59cc69c0c647a2dd99b3
JuampaCV <126704694+JuampaCV@users.noreply.github.com>
2023-04-19 11:56:40 -0300

    Add files via upload

---
+++ Python juan y dante/T_19.py
@@ -0,0 +1,6 @@
+num1= int (input ("Ingresar un numero positivo"))
+x=0
+while x <num1:
+    x=x+1
+    print(x)
+    
---
+++ Python juan y dante/T_20.py
@@ -0,0 +1,13 @@
+num1= int (input ("Ingresar un valor"))
+num2= int (input ("Ingresar un valor"))
+num3= int (input ("Ingresar un valor"))
+num4= int (input ("Ingresar un valor"))
+num5= int (input ("Ingresar un valor"))
+num6= int (input ("Ingresar un valor"))
+num7= int (input ("Ingresar un valor"))
+num8= int (input ("Ingresar un valor"))
+num9= int (input ("Ingresar un valor"))
+num10= int (input ("Ingresar un valor"))
+suma= num1+num2+num3+num4+num5+num6+num7+num8+num9+num10
+promedio=suma/10
+input("La suma de los numeros es: ",suma "y el promedio es", promedio)
